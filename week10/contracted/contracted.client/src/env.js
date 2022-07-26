0a5b857376e1540f5773975e38f653d29f8667ca
Patrick Misner <misner.patrick@gmail.com>
2022-07-26 14:21:10 -0600

    server

--- week10/contracted/contracted.client/src/env.js
+++ week10/contracted/contracted.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-in-9pjrc.us.auth0.com'
+export const audience = 'https://patrickdev.com'
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const clientId = 'KCkMZ4ZIRzme4LvLXEQoEorQQItsONAx'
